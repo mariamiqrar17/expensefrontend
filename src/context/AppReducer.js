c473d6abadf2afd6440b33da87fa03b936a73d76
mariamiqrar17 <mariam_iqrar_intern@netixsol.com>
2024-01-02 10:48:21 +0500

    change

---
+++ src/context/AppReducer.js
@@ -0,0 +1,31 @@
+// Give your reducer function a name
+const appReducer = (state, action) => {
+  switch(action.type) {
+    case 'GET_TRANSACTIONS':
+      return {
+        ...state,
+        loading: false,
+        transactions: action.payload
+      }
+    case 'DELETE_TRANSACTION':
+      return {
+        ...state,
+        transactions: state.transactions.filter(transaction => transaction._id !== action.payload)
+      }
+    case 'ADD_TRANSACTION':
+      return {
+        ...state,
+        transactions: [...state.transactions, action.payload]
+      }
+    case 'TRANSACTION_ERROR':
+      return {
+        ...state,
+        error: action.payload
+      }
+    default:
+      return state;
+  }
+}
+
+// Export the named function as the default export
+export default appReducer;
